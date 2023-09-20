55074267e658af91df8a3100dbc518b79f68072b
Han-Enc <h01062598017@gmail.com>
2023-09-20 10:51:38 +0900

    third

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,15 +15,6 @@ const Header = () => {
           </div>
 
           <div className="flex items-center font-bold">
-            <Link
-              href="/repos"
-              className="text-2xl text-gray-300 hover:text-white mr-4"
-            >
-              Repos
-            </Link>
-          </div>
-
-          <div className="flex items-center font-bold">
             {!userId ? (
               // 로그인이 안된 경우
               <>
@@ -44,6 +35,20 @@ const Header = () => {
             ) : (
               // 로그인이 된 경우
               <>
+                <Link
+                  href="/repos"
+                  className="text-gray-300 hover:text-white mr-4"
+                >
+                  Repos
+                </Link>
+
+                <Link
+                  href="/courses"
+                  className="text-gray-300 hover:text-white mr-4"
+                >
+                  Courses
+                </Link>
+
                 <Link
                   href="/dashboard"
                   className="text-gray-300 hover:text-white mr-4"
