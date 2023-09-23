b54626463aa44568b57d1610a2aba1604def6c27
Han-Enc <h01062598017@gmail.com>
2023-09-23 20:29:37 +0900

    forth

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
+      <p>Hello! Wellcome to this web page!</p>
     </>
-  )
+  );
 }
